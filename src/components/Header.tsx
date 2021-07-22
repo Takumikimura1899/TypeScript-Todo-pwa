78f76313a70435180b0688c0dddae422282d7e56
Takumikimura141899 <kimura141899@gmail.com>
2021-07-23 05:27:53 +0900

    material_ui_header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import { signInWithGoogle, logOut } from '../service/Firebase';
 import { AuthContext } from '../provider/AuthProvider';
 import { useContext } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header: React.FC = () => {
   // useContextを使用してAuthContextから値を取り出す。
@@ -16,20 +22,22 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header>
-      ヘッダー
-      <br />
-      <p>関数宣言型</p>
-      {buttonRender()}
-      <br />
-      <p>三項演算子</p>
-      {currentUser.currentUser ? (
-        <button onClick={logOut}>ログアウト</button>
-      ) : (
-        <button onClick={signInWithGoogle}>ログイン</button>
-      )}
-      <br />
-    </header>
+    <AppBar>
+      <Toolbar>
+        <Typography>ヘッダー</Typography>
+        <br />
+        <p>関数宣言型</p>
+        {buttonRender()}
+        <br />
+        <p>三項演算子</p>
+        {currentUser.currentUser ? (
+          <button onClick={logOut}>ログアウト</button>
+        ) : (
+          <button onClick={signInWithGoogle}>ログイン</button>
+        )}
+        <br />
+      </Toolbar>
+    </AppBar>
   );
 };
 
